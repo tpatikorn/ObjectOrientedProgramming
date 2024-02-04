b3cefab8afb9fc2904b3397f7f9febf094c404e4
Thanaporn Patikorn <tpatikorn@gmail.com>
2024-02-04 16:33:21 +0700

    updated football

--- ObjectOrientedProgramming/src/week3/Football.java
+++ ObjectOrientedProgramming/src/week3/Football.java
@@ -2,12 +2,15 @@ package week3;
 
 import java.util.ArrayList;
 
-public class Football extends Sport {
+public class Football extends Sport { // inherit from Sport
 
 	
 	public Football(ArrayList<String> team1PlayerNames, 
 			ArrayList<String> team2PlayerNames, Result result) {
 		super(team1PlayerNames, team2PlayerNames, result);
+		this.name = "Football";
+		this.rule = "punch the face = WIN";
+		System.out.println(super.getNumber());
 	}
 
 	public static void main(String args[]) {
@@ -16,8 +19,17 @@ public class Football extends Sport {
 		
 		ArrayList<String> names2 = new ArrayList<String>();
 		names2.add("Ronaldo2");
-		Football s = new Football(names1, names2, Result.DRAW);
+		Football newFootball = new Football(names1, names2, Result.DRAW);
+		System.out.println(newFootball.team1PlayerNames);
 		
-		System.out.println(s.result);
+		names1.add("Hello World");
+		names2.add("xxxxxxxxxxx");
+		System.out.println(names1);
+		System.out.println(names2);
+		System.out.println(newFootball.team1PlayerNames);
+		System.out.println(newFootball.team2PlayerNames);
+		
+		System.out.println(newFootball.getName());
+		System.out.println(newFootball.getResult());
 	}
 }
